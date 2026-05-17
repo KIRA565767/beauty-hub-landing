4780177a41b1226576a4f850b414799f04aa9707
KIRA565767 <153532800+KIRA565767@users.noreply.github.com>
2026-05-17 17:03:36 +0300

    Add route hall before product routes

--- src/beauty-hub/components/pages/RoutesPage.jsx
+++ src/beauty-hub/components/pages/RoutesPage.jsx
@@ -1,9 +1,11 @@
 import { FinalConversionSection } from "../sections/FinalConversionSection";
 import { ProductSystemSection } from "../sections/ProductSystemSection";
+import { RouteHallIntroSection } from "../sections/RouteHallIntroSection";
 
 export function RoutesPage() {
   return (
     <>
+      <RouteHallIntroSection />
       <ProductSystemSection />
       <FinalConversionSection />
     </>
