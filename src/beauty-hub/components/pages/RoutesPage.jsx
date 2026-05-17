ab47243c55cf1ed48ded2445f61c91a7c3803b00
KIRA565767 <153532800+KIRA565767@users.noreply.github.com>
2026-05-17 16:20:50 +0300

    Add routes hash page

---
+++ src/beauty-hub/components/pages/RoutesPage.jsx
@@ -0,0 +1,11 @@
+import { FinalConversionSection } from "../sections/FinalConversionSection";
+import { ProductSystemSection } from "../sections/ProductSystemSection";
+
+export function RoutesPage() {
+  return (
+    <>
+      <ProductSystemSection />
+      <FinalConversionSection />
+    </>
+  );
+}
